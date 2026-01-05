b7d87b1d01bd7db91ca17f5269abe6d365f3e646
Mingrui-Yu <1249682625@qq.com>
2026-01-05 15:23:12 +0800

    support direct load of  xml file in addition to load of xml string

--- src/pytorch_kinematics/mjcf.py
+++ src/pytorch_kinematics/mjcf.py
@@ -1,5 +1,5 @@
 from typing import Union
-
+import os
 import mujoco
 from mujoco._structs import _MjModelBodyViews as MjModelBodyViews
 
@@ -8,10 +8,7 @@ from . import chain
 from . import frame
 
 # Converts from MuJoCo joint types to pytorch_kinematics joint types
-JOINT_TYPE_MAP = {
-    mujoco.mjtJoint.mjJNT_HINGE: 'revolute',
-    mujoco.mjtJoint.mjJNT_SLIDE: "prismatic"
-}
+JOINT_TYPE_MAP = {mujoco.mjtJoint.mjJNT_HINGE: "revolute", mujoco.mjtJoint.mjJNT_SLIDE: "prismatic"}
 
 
 def body_to_geoms(m: mujoco.MjModel, body: MjModelBodyViews):
@@ -20,8 +17,11 @@ def body_to_geoms(m: mujoco.MjModel, body: MjModelBodyViews):
     for geom_id in range(m.ngeom):
         geom = m.geom(geom_id)
         if geom.bodyid == body.id:
-            visuals.append(frame.Visual(offset=tf.Transform3d(rot=geom.quat, pos=geom.pos), geom_type=geom.type,
-                                        geom_param=geom.size))
+            visuals.append(
+                frame.Visual(
+                    offset=tf.Transform3d(rot=geom.quat, pos=geom.pos), geom_type=geom.type, geom_param=geom.size
+                )
+            )
     return visuals
 
 
@@ -38,14 +38,20 @@ def _build_chain_recurse(m, parent_frame, parent_body):
                 # Find the joint for this body, again assuming there's only one joint per body.
                 joint = m.joint(body.jntadr[0])
                 joint_offset = tf.Transform3d(pos=joint.pos)
-                child_joint = frame.Joint(joint.name, offset=joint_offset, axis=joint.axis,
-                                          joint_type=JOINT_TYPE_MAP[joint.type[0]],
-                                          limits=(joint.range[0], joint.range[1]))
+                child_joint = frame.Joint(
+                    joint.name,
+                    offset=joint_offset,
+                    axis=joint.axis,
+                    joint_type=JOINT_TYPE_MAP[joint.type[0]],
+                    limits=(joint.range[0], joint.range[1]),
+                )
             else:
                 child_joint = frame.Joint(body.name + "_fixed_joint")
             child_link = frame.Link(body.name, offset=tf.Transform3d(rot=body.quat, pos=body.pos))
             child_frame = frame.Frame(name=body.name, link=child_link, joint=child_joint)
-            parent_frame.children = parent_frame.children + [child_frame, ]
+            parent_frame.children = parent_frame.children + [
+                child_frame,
+            ]
             _build_chain_recurse(m, child_frame, body)
 
     # iterate through all sites that are children of parent_body
@@ -54,7 +60,9 @@ def _build_chain_recurse(m, parent_frame, parent_body):
         if site.bodyid == parent_body.id:
             site_link = frame.Link(site.name, offset=tf.Transform3d(rot=site.quat, pos=site.pos))
             site_frame = frame.Frame(name=site.name, link=site_link)
-            parent_frame.children = parent_frame.children + [site_frame, ]
+            parent_frame.children = parent_frame.children + [
+                site_frame,
+            ]
 
 
 def build_chain_from_mjcf(data, body: Union[None, str, int] = None):
@@ -73,15 +81,20 @@ def build_chain_from_mjcf(data, body: Union[None, str, int] = None):
     chain.Chain
         Chain object created from MJCF.
     """
-    m = mujoco.MjModel.from_xml_string(data)
+    if data.endswith(".xml"):
+        m = mujoco.MjModel.from_xml_path(data)
+    else:
+        m = mujoco.MjModel.from_xml_string(data)
+
     if body is None:
         root_body = m.body(0)
     else:
         root_body = m.body(body)
-    root_frame = frame.Frame(root_body.name,
-                             link=frame.Link(root_body.name,
-                                             offset=tf.Transform3d(rot=root_body.quat, pos=root_body.pos)),
-                             joint=frame.Joint())
+    root_frame = frame.Frame(
+        root_body.name,
+        link=frame.Link(root_body.name, offset=tf.Transform3d(rot=root_body.quat, pos=root_body.pos)),
+        joint=frame.Joint(),
+    )
     _build_chain_recurse(m, root_frame, root_body)
     return chain.Chain(root_frame)
 
